75d5b8373988d09fb291dc59f3b086010be79346
Artem Borisovskiy <bytefu@gmail.com>
2017-10-14 13:57:32 +0200

    Log item titles

--- fetcher.cpp
+++ fetcher.cpp
@@ -1,21 +1,23 @@
 #include <sstream>
 #include <fstream>
 #include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
 #include <mgr/mgrxml.h>
 #include <mgr/mgrlog.h>
 #include <ispbin.h>
 #include <curl/curl.h>
-#include <cstdio>
-#include <cstdlib>
 
 using namespace std;
 using namespace mgr_log;
+using namespace mgr_xml;
 
 #define BINARY_NAME "rssfetcher"
 MODULE(BINARY_NAME);
 
 stringstream rssResponse;
-extern "C" size_t curl_writefunction(char *ptr, size_t size, size_t nmemb, void *userdata)
+extern "C" size_t responseWriter(char *ptr, size_t size, size_t nmemb, void *userdata)
 {
 	auto totalSize = size * nmemb;
 	rssResponse.write(ptr, totalSize);
@@ -24,29 +26,31 @@ extern "C" size_t curl_writefunction(char *ptr, size_t size, size_t nmemb, void 
 
 int ISP_MAIN(const int argc, char *argv[])
 {
-	mgr_log::Init(BINARY_NAME);
+	mgr_log::Init(BINARY_NAME, L_DEBUG);
 	LogInfo("Invoked as %s", argv[0]);
-
-	// We don't output any data, just make the core happy
-	//cout << mgr_xml::Xml().Str();
 
 	auto curl = curl_easy_init();
 	if (!curl) {
 		return EXIT_FAILURE;
 	}
 	curl_easy_setopt(curl, CURLOPT_URL, "http://feeds.rucast.net/radio-t");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_writefunction);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, responseWriter);
     curl_easy_perform(curl);
     curl_easy_cleanup(curl);
 
     rssResponse.seekg(0, ios::end);
-    printf("### RESPONSE SIZE %zu bytes\n", static_cast<size_t>(rssResponse.tellg()));
+    const auto responseSize = static_cast<size_t>(rssResponse.tellg());
     rssResponse.seekg(0, ios::beg);
+    Debug("Got RSS response (%zu bytes)", static_cast<size_t>(responseSize));
 
-    //cout << rssResponse.rdbuf();
+    auto rssXml = Xml(rssResponse);
+    auto items = rssXml.GetNodes("/rss/channel/item");
+    Debug("Fetched %zu items", items.size());
+    for_each(items.begin(), items.end(), [] (const XmlNode &item) {
+		Debug("item '%s'", item.FindNode("title").Str().c_str());
+	});
 
-    auto rssXml = mgr_xml::Xml(rssResponse);
-    cout << rssXml.Str();
+	cout << Xml().Str();
 
 	return EXIT_SUCCESS;
 }
